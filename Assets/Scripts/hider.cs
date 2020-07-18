63dc56f4b36a15f8a83e9193b926decc46ebf75e
Maktastic <63370820+Maktastic@users.noreply.github.com>
2020-07-18 16:01:40 +0400

    Internship

---
+++ Assets/Scripts/hider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hider : MonoBehaviour
+{
+    public Text t;
+
+    void Start()
+    {
+        Invoke("DisableText", 2f);
+        print("Welcome screen is hidden");
+    }
+
+    void DisableText()
+    {
+        t.enabled = false;
+    }
+}
---
+++ Assets/Scripts/rot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rot : MonoBehaviour
+{
+
+    protected bool rotate = false;
+    public float x, y, z;
+    public Quaternion org_rot; 
+    float speed = 0.25f;
+
+    public void OnMouseDown()
+    {
+  
+        rotate = true;
+
+    }
+
+    public void stop_rotate()
+    {
+        rotate = false;
+        transform.rotation = Quaternion.Slerp(transform.rotation, org_rot, Time.time * speed);
+    }
+
+    public void Update()
+    {
+        if (rotate)
+        {
+            transform.Rotate(new Vector3(x, y, z) * Time.deltaTime);
+        }
+        
+    }
+}
